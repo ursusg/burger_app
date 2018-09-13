5b43c7d9072d8990a66e36beafae755829177394
Ursus <ugurungsus@gmail.com>
2018-09-12 21:14:25 -0500

    adding files

---
+++ config/connection.js
@@ -0,0 +1,34 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+require('dotenv').config();
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB
+});
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  })
+}
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
