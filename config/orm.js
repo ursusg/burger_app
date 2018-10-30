8158bc0148b6f05dce5336200be44d37212ed495
Ursus <ugurungsus@gmail.com>
2018-10-30 16:38:01 -0500

    updated files

--- config/orm.js
+++ config/orm.js
@@ -70,7 +70,6 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -103,5 +102,5 @@ var orm = {
   }
 };
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for the model (burger.js).
 module.exports = orm;
